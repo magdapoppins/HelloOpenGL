d47d248e2a92d96634e74c6b4c3e6386006f19eb
Magda Stenius <mmbste@utu.fi>
2018-09-14 10:17:40 +0300

    Vertices in buffer with attributes.

--- HelloOpenGL/Application.cpp
+++ HelloOpenGL/Application.cpp
@@ -36,6 +36,21 @@ int main(void)
 	if (glewInit() != GLEW_OK)
 		std::cout << "Error on glewInit()" << std::endl;
 	
+	// Define three vertice positions
+	float positions[6] = {
+		-0.5f, -0.5f, // Each line represents a vertex position (x and y values)
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+
+	// Pass in the memory address of an unsigned int (this will be the return value)
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -43,12 +58,17 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Legacy GL to test all is up and running
-		glBegin(GL_TRIANGLES);
-		glVertex2d(-0.5f, -0.5f);
-		glVertex2d(0.0f, 0.5f);
-		glVertex2d(0.5f, -0.5f);
-		glEnd();
+		//// Legacy GL to test all is up and running
+		//glBegin(GL_TRIANGLES);
+		//glVertex2d(-0.5f, -0.5f);
+		//glVertex2d(0.0f, 0.5f);
+		//glVertex2d(0.5f, -0.5f);
+		//glEnd();
+
+		// Draws from the glBindBuffer!
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// glDrawElements(GL_TRIANGLES, 3, ) This is with index buffers
+
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
