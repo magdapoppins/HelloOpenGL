677c23f564409daff05d8a8b475f27de7c509a8e
Magda Stenius <mmbste@utu.fi>
2018-09-14 13:48:02 +0300

    First sample shaders: vertexshader and fragmentshader.

--- HelloOpenGL/Application.cpp
+++ HelloOpenGL/Application.cpp
@@ -1,24 +1,56 @@
-// LIFE CHECK:
- #include <iostream>
-
-//int main() {
-//	std::cout << "Hello" << std::endl;
-//	std::cin.get();
-//}
-
+#include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+static unsigned int CompileShader(const std::string& source, unsigned int type) {
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) 
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " << 
+			(type == GL_VERTEX_SHADER ? "vertex" : "fragment") 
+			<< " shader" << std::endl;
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(vertexShader, GL_VERTEX_SHADER);
+	unsigned int fs = CompileShader(fragmentShader, GL_FRAGMENT_SHADER);
+	
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program; 
+}
+
 int main(void)
 {
-	// Sample code from GLFW --->
 	GLFWwindow* window;
 
-	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
 
-	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello OpenGl", NULL, NULL);
 	if (!window)
 	{
@@ -26,24 +58,19 @@ int main(void)
 		return -1;
 	}
 
-	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
-	// Print out version
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	// This needs to happen after we have a valid context, see more in http://glew.sourceforge.net/basic.html
 	if (glewInit() != GLEW_OK)
 		std::cout << "Error on glewInit()" << std::endl;
 	
-	// Define three vertice positions
 	float positions[6] = {
-		-0.5f, -0.5f, // Each line represents a vertex position (x and y values)
+		-0.5f, -0.5f, 
 		 0.0f,  0.5f,
 		 0.5f, -0.5f
 	};
 
-	// Pass in the memory address of an unsigned int (this will be the return value)
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
@@ -52,28 +79,44 @@ int main(void)
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-	/* Loop until the user closes the window */
+	// Fragmentshader 
+	std::string fragmentShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) out vec4 color;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n" // R G B A
+		"}\n"
+		"\n"
+		"\n";
+
+	// Vertexshader 
+	std::string vertexShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = position;\n"
+		"}\n"
+		"\n"
+		"\n";
+
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
+
+
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//// Legacy GL to test all is up and running
-		//glBegin(GL_TRIANGLES);
-		//glVertex2d(-0.5f, -0.5f);
-		//glVertex2d(0.0f, 0.5f);
-		//glVertex2d(0.5f, -0.5f);
-		//glEnd();
-
-		// Draws from the glBindBuffer!
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-		// glDrawElements(GL_TRIANGLES, 3, ) This is with index buffers
 
-
-		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
-		/* Poll for and process events */
 		glfwPollEvents();
 	}
 
